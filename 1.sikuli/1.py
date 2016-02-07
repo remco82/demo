c5762986d281d1004e964e1d4faeb0de947d1f91
remco hiddema <remco hiddema>
2016-02-07 18:29:45 +0100

    second commit

---
+++ 1.sikuli/1.py
@@ -0,0 +1,13 @@
+doubleClick("1454848831304.png")
+click(Pattern("1454848860852.png").targetOffset(41,-2))
+click("1454848991736.png")
+sleep(5)
+click("1454849029264.png")
+wait("1454849522805.png")
+click(Pattern("1454849668867.png").similar(0.29).targetOffset(28,-13))
+click(Pattern("1454849704289.png").targetOffset(39,-40))
+find("1454849752497.png")
+
+
+
+

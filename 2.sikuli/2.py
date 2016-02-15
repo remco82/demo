59e13c2dcb0842c1db3aa212efdc56ac6a07387c
remco hiddema <remco hiddema>
2016-02-15 10:01:41 +0100

    Third commit; created new testcase to calculate 1+1=2

---
+++ 2.sikuli/2.py
@@ -0,0 +1,10 @@
+find("1455300734580.png")
+doubleClick("1455300734580.png")
+sleep(5)
+click("1455300902379.png")
+
+click("1455300936751.png")
+click("1455300902379.png")
+click("1455301109352.png")
+find("1455301156377.png")
+click(Pattern("1455301213443.png").targetOffset(40,0))

55e86942dcec91d634bdf9d164cc26bcfd488e1d
remco hiddema <remco hiddema>
2016-02-15 10:08:27 +0100

    fourth commit, now 2+2=4

--- 2.sikuli/2.py
+++ 2.sikuli/2.py
@@ -1,10 +1,9 @@
 find("1455300734580.png")
 doubleClick("1455300734580.png")
 sleep(5)
-click("1455300902379.png")
-
+click("1455527172376.png")
 click("1455300936751.png")
-click("1455300902379.png")
+click("1455527172376.png")
 click("1455301109352.png")
-find("1455301156377.png")
+find("1455527203892.png")
 click(Pattern("1455301213443.png").targetOffset(40,0))
